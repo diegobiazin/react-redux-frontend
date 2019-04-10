4db04e599b001f3e5f5de3daa628ec4181f215a4
Diego Biazin <diego_biazin@hotmail.com>
2019-04-09 21:00:50 -0300

    Componente BillingCycleList

--- src/billingCycle/billingCycle.jsx
+++ src/billingCycle/billingCycle.jsx
@@ -2,7 +2,6 @@ import React, { Component } from 'react'
 import { bindActionCreators } from 'redux'
 import { connect } from 'react-redux'
 
-
 import ContentHeader from '../common/template/contentHeader'
 import Content from '../common/template/content'
 import Tabs from '../common/tab/tabs'
@@ -12,6 +11,7 @@ import TabHeader from '../common/tab/tabHeader'
 import TabContent from '../common/tab/tabContent'
 import { selectTab, showTabs } from '../common/tab/tabActions'
 
+import List from './billingCycleList'
 class BillingCycle extends Component {
 
   componentWillMount() {
@@ -37,7 +37,7 @@ class BillingCycle extends Component {
             </TabsHeader>
             <TabsContent>
               <TabContent id='tabList'>
-                <h1>Lista</h1>
+                <List />
               </TabContent>
               <TabContent id='tabCreate'>
                 <h1>Incluir</h1>
@@ -56,5 +56,6 @@ class BillingCycle extends Component {
   }
 }
 
-const mapDispatchToProps = dispatch => bindActionCreators({ selectTab, showTabs }, dispatch)
+const mapDispatchToProps = dispatch => bindActionCreators({ selectTab, showTabs },
+  dispatch)
 export default connect(null, mapDispatchToProps)(BillingCycle)
