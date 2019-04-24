11992f9d7cfbd08f3c22bef55f12bb2f47b8c71d
Diego Biazin <diego_biazin@hotmail.com>
2019-04-23 22:15:56 -0300

    Integrando BillingCycleList com Redux

--- src/billingCycle/billingCycleList.jsx
+++ src/billingCycle/billingCycleList.jsx
@@ -1,7 +1,16 @@
 import React, { Component } from 'react'
+import { bindActionCreators } from 'redux'
+import { connect } from 'react-redux'
+import { getList, showUpdate, showDelete } from './billingCycleActions'
 
 class BillingCycleList extends Component {
+
+    componentWillMount() {
+        this.props.getList()
+    }
+
     render() {
+        console.log(this.props.getList())
         return (
             <div>
                 <table className='table'>
@@ -19,4 +28,7 @@ class BillingCycleList extends Component {
         )
     }
 }
-export default BillingCycleList
+
+const mapStateToProps = state => ({ list: state.billingCycle.list })
+const mapDispatchToProps = dispatch => bindActionCreators({ getList }, dispatch)
+export default connect(mapStateToProps, mapDispatchToProps)(BillingCycleList)
