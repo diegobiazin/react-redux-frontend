d73ce744ddb3e3735301b259594c1e31ea9b599a
Diego Biazin <diego_biazin@hotmail.com>
2019-04-26 15:15:39 -0300

    BillingCycleForme com Redux-Form

--- src/billingCycle/billingCycle.jsx
+++ src/billingCycle/billingCycle.jsx
@@ -10,8 +10,11 @@ import TabsContent from '../common/tab/tabsContent'
 import TabHeader from '../common/tab/tabHeader'
 import TabContent from '../common/tab/tabContent'
 import { selectTab, showTabs } from '../common/tab/tabActions'
+import { create } from './billingCycleActions'
 
 import List from './billingCycleList'
+import Form from './billingCycleForm'
+
 class BillingCycle extends Component {
 
   componentWillMount() {
@@ -40,7 +43,7 @@ class BillingCycle extends Component {
                 <List />
               </TabContent>
               <TabContent id='tabCreate'>
-                <h1>Incluir</h1>
+                <Form onSubmit={this.props.create} />
               </TabContent>
               <TabContent id='tabUpdate'>
                 <h1>Alterar</h1>
@@ -56,6 +59,7 @@ class BillingCycle extends Component {
   }
 }
 
-const mapDispatchToProps = dispatch => bindActionCreators({ selectTab, showTabs },
-  dispatch)
+const mapDispatchToProps = dispatch => bindActionCreators({
+  selectTab, showTabs, create
+}, dispatch)
 export default connect(null, mapDispatchToProps)(BillingCycle)
--- src/main/reducers.js
+++ src/main/reducers.js
@@ -1,4 +1,5 @@
 import { combineReducers } from "redux";
+import { reducer as formReducer } from 'redux-form'
 
 import DashboardReducer from "../dashboard/dashboardReducer";
 import TabReducer from '../common/tab/tabReducer'
@@ -8,7 +9,8 @@ const rootReducer = combineReducers({
   //dashboard: () => ({ summary: { credit: 100, debt: 50 } })
   dashboard: DashboardReducer,
   tab: TabReducer,
-  billingCycle: BillingCycleReducer
+  billingCycle: BillingCycleReducer,
+  form: formReducer
 });
 
 export default rootReducer;
