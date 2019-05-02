9c190da8dc66b08107474b2f683e5e82695866d3
Diego Biazin <diego_biazin@hotmail.com>
2019-05-02 14:54:49 -0300

    Adicionando valores de debito

--- src/billingCycle/itemList.jsx
+++ src/billingCycle/itemList.jsx
@@ -4,6 +4,7 @@ import { bindActionCreators } from 'redux'
 import { Field, arrayInsert, arrayRemove } from 'redux-form'
 import Grid from '../common/layout/grid'
 import Input from '../common/form/input'
+import If from '../common/operator/if'
 
 class ItemList extends Component {
 
@@ -24,11 +25,23 @@ class ItemList extends Component {
         return list.map((item, index) => (
             <tr key={index}>
                 <td><Field name={`${this.props.field}[${index}].name`} component={Input}
-                    placeholder='Informe o nome' readOnly={this.props.readOnly}
-                /></td>
+                    placeholder='Informe o nome' readOnly={this.props.readOnly} />
+                </td>
                 <td><Field name={`${this.props.field}[${index}].value`} component={Input}
-                    placeholder='Informe o valor' readOnly={this.props.readOnly}
-                /></td>
+                    placeholder='Informe o valor' readOnly={this.props.readOnly} />
+                </td>
+                <If test={this.props.showStatus}>
+                    <td>
+                        <Field name={`${this.props.field}[${index}].status`}
+                            component={Input}
+                            placeholder='Informe o status' readOnly={this.props.readOnly}>
+                            <option></option>
+                            <option value="PAGO">Pago</option>
+                            <option value="PENDENTE">Pendente</option>
+                            <option value="AGENDADO">Agendado</option>
+                        </Field>
+                    </td>
+                </If>
                 <td>
                     <button type='button' className='btn btn-success'
                         onClick={() => this.add(index + 1)}>
@@ -57,6 +70,9 @@ class ItemList extends Component {
                             <tr>
                                 <th>Nome</th>
                                 <th>Valor</th>
+                                <If test={this.props.showStatus}>
+                                    <th>Status</th>
+                                </If>
                                 <th className='table-actions'>Ações</th>
                             </tr>
                         </thead>
